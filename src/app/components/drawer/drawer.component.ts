def07573585708b80c7c6767ec86048c1731b0c4
ThomasBalaban <balabaninteractive@gmail.com>
2026-03-05 23:30:51 -0500

    missed one

--- src/app/components/drawer/drawer.component.ts
+++ src/app/components/drawer/drawer.component.ts
@@ -9,7 +9,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   template: `
     <div class="debug-nav">
       <a routerLink="/" routerLinkActive="active" [routerLinkActiveOptions]="{ exact: true }"
-         class="nav-btn nav-link" title="Dashboard">&#127968;</a>
+         class="nav-btn nav-link" title="Dashboard">🏠</a>
       <a routerLink="/sensors" routerLinkActive="active"
          class="nav-btn nav-link" title="Raw Sensor Feeds"><span _ngcontent-ng-c3811861080="" class="title-icon">🔭</span></a>
       <a routerLink="/brain" routerLinkActive="active"
